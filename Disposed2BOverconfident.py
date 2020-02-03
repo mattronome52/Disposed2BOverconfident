3fa19ec7d0f42010ba16896ab2e983d228d42ccc
Matt Morse <matt@mac.com>
2020-02-02 16:53:16 -0800

    Merged my fixes with Katrin's
    Fixed description problems and merged Katrin's new buy strategy code.

--- Disposed2BOverconfident.py
+++ Disposed2BOverconfident.py
@@ -41,73 +41,6 @@ class Market(object):
 # In[2]:
 
 
-from enum import Enum
-import random
-
-class BuyStrategy(Enum):
-  RANDOM = 1
-  BUY_GAINERS = 2 # stocks with current price > starting price
-  BUY_LOSERS  = 3 # stocks with current price > starting price 
-
-class SellStrategy(Enum):
-  RANDOM = 1
-  SELL_GAINERS = 2 
-  SELL_LOSERS  = 3 
-
-class Investor:
-  def __init__(self, name, buyStrategy, sellStrategy):
-    self.name = name
-    self.allStocks = []
-
-    if (buyStrategy in BuyStrategy.__members__):
-      self.buyStrategy  = buyStrategy
-    else:
-      print(f'{buyStrategy} is not a valid buying strategy')
-
-    if (sellStrategy in SellStrategy.__members__):
-      self.sellStrategy  = sellStrategy
-    else:
-      print(f'{sellStrategy} is not a valid selling strategy')
-
-  def name(self):
-    return self.name
-
-  def portfolio(self):
-    return self.allStocks
-
-  def addStockToPortfolio(self, stock):
-    self.allStocks.append(stock)
-    
-  def createInitialPortfolioWithNumStocks(self, numStocks):
-    # need to test numStocks is within bounds
-    # Matt: for now, just implementing random strategy
-    if (self.buyStrategy is BuyStrategy.RANDOM.name):
-      self.allStocks = random.sample(Market.initialStocks, numStocks)
-    else:
-      # Matt: add conditions and code for other stradegies here
-      print ("Under Construction: Only the RANDOM stategy is implemented at this time")
-    
-  def buyStrategy(self):
-    return self.buyStrategy
-
-  def sellStrategy(self):
-    return self.sellStrategy
-
-  def description(self):    
-    print(f'Investor: {self.name}')
-    print(f'  buy strategy:  {self.buyStrategy}')
-    print(f'  sell strategy: {self.sellStrategy}')
-    
-    if (len(self.allStocks) == 0):
-      print("    No stocks in portfolio")
-    else:
-      for stock in self.allStocks[:]:
-        stock.description()
-
-
-# In[3]:
-
-
 from random import choices
 
 ####### Constants ######
@@ -160,6 +93,10 @@ class Stock(object):
         
     return self.initialPrice() + sum(priceChangeHistoryForTest[0:periodNum])
 
+  def gainsPrevious (self):
+      numberGainsPrevious = sum(1 for priceChangePrev in self.priceChangeHistory[:3] if priceChangePrev > 0)
+      return numberGainsPrevious
+
   def description(self):    
     print(f'Stock: {self.name}')
     print(f'  quality:                  {self.quality}')
@@ -169,6 +106,88 @@ class Stock(object):
       print(f'  price for current period: {self.initialPrice}')
     else:
       print(f'  price for current period: {self.priceForTestPeriod(self.marketClass.currentPeriod)}')
+
+
+# In[3]:
+
+
+from enum import Enum
+import random
+
+class BuyStrategy(Enum):
+  RANDOM = 1
+  BUY_GAINERS = 2 # stocks with current price > starting price
+  BUY_LOSERS  = 3 # stocks with current price > starting price 
+
+class SellStrategy(Enum):
+  RANDOM = 1
+  SELL_GAINERS = 2 
+  SELL_LOSERS  = 3
+
+class Investor:
+  def __init__(self, name, buyStrategy, sellStrategy):
+    self.name = name
+    self.portfolio = []
+
+    if (buyStrategy in BuyStrategy.__members__):
+      self.buyStrategy  = buyStrategy
+    else:
+      print(f'{buyStrategy} is not a valid buying strategy')
+
+    if (sellStrategy in SellStrategy.__members__):
+      self.sellStrategy  = sellStrategy
+    else:
+      print(f'{sellStrategy} is not a valid selling strategy')
+
+  def name(self):
+    return self.name
+
+  def portfolio(self):
+    return self.portfolio
+
+  def addStockToPortfolio(self, stock):
+    self.portfolio.append(stock)
+    
+  def createInitialPortfolioWithNumStocks(self, numStocks):
+    # need to test numStocks is within bounds
+    # Matt: for now, just implementing random strategy
+    if (self.buyStrategy is BuyStrategy.RANDOM.name):
+      self.portfolio = random.sample(Market.initialStocks, numStocks)
+    elif (self.buyStrategy is BuyStrategy.BUY_GAINERS.name):
+      # Katrin: Pick five stock at random and then go through market stocks and replace if more gains in previous periods
+      stockMaxPrevGains = random.sample(Market.initialStocks, numStocks)
+      i = 0
+      while (i < (len(Market.initialStocks)-2)):
+        currentGains = Market.initialStocks[i].gainsPrevious()
+        j = 0
+        while (j < numStocks):
+          if (currentGains > stockMaxPrevGains[j].gainsPrevious()):
+            stockMaxPrevGains[j] = Market.initialStocks[i]
+            break
+          j = j+1
+        i = i+1
+      self.portfolio = stockMaxPrevGains
+    else:
+      # Matt: add conditions and code for other stradegies here
+      # Katrin: I added the buying gainers strategy. We do not need a buying losers strategy.
+      print ("Invalid strategy")
+    
+  def buyStrategy(self):
+    return self.buyStrategy
+
+  def sellStrategy(self):
+    return self.sellStrategy
+
+  def description(self):    
+    print(f'Investor: {self.name}')
+    print(f'  buy strategy:  {self.buyStrategy}')
+    print(f'  sell strategy: {self.sellStrategy}')
+    
+    if (len(self.portfolio) == 0):
+      print("    No stocks in portfolio")
+    else:
+      for stock in self.portfolio[:]:
+        stock.description()
 
 
 # In[4]:
@@ -181,25 +200,29 @@ market.description()
 # In[5]:
 
 
-investor = Investor("firstInvestor", 'RANDOM', 'RANDOM')
+investor1 = Investor("firstInvestor", 'RANDOM', 'RANDOM')
+investor2 = Investor("secondInvestor", 'BUY_GAINERS', 'RANDOM')
 
 
 # In[6]:
 
 
-investor.description()
+investor1.description()
+investor2.description()
 
 
 # In[7]:
 
 
-investor.createInitialPortfolioWithNumStocks(5)
+investor1.createInitialPortfolioWithNumStocks(5)
+investor2.createInitialPortfolioWithNumStocks(5)
 
 
 # In[8]:
 
 
-investor.description()
+investor1.description()
+investor2.description()
 
 
 # In[ ]:
